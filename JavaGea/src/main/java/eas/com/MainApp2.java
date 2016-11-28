71267d5e7c80fa7381b2efe4d5bbbbe41bc9c396
Eduardo Alfonso Sanchez <eddie.alfonso@gmail.com>
2016-11-28 07:06:16 -0500

    Update MainApp2.java

--- JavaGea/src/main/java/eas/com/MainApp2.java
+++ JavaGea/src/main/java/eas/com/MainApp2.java
@@ -15,15 +15,17 @@ import java.io.File;
 public class MainApp2 {
 
     public static void main(String[] args) throws JAXBException {
-
+        /* File for Reading */
         File file = new File("author.xml");
 
+        /* init jaxb unmarshaller */
         JAXBContext jaxbContext = JAXBContext.newInstance(Author.class);
-
         Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 
+        /* unmarshaller xml to java object */
         Author author = (Author) jaxbUnmarshaller.unmarshal(file);
 
+        /* print java object*/
         System.out.println(author);
     }
 }
