5089d7be3d7ff7eaae9d5953fce3af832d64d556
Eduardo Alfonso Sanchez <eddie.alfonso@gmail.com>
2019-09-26 10:33:16 -0500

    add example of method invoke by reflection

--- java-reflection-method/src/main/java/eas/com/SomeClass.java
+++ java-reflection-method/src/main/java/eas/com/SomeClass.java
@@ -2,7 +2,7 @@ package eas.com;
 
 public class SomeClass implements SomeInterface {
     @Override
-    public String someMethod(String param1, String param2, String param3) {
-        return param1 + param2 + param3;
+    public String someMethod(String param1, String param2, String param3, String param4) {
+        return param1 + " " + param2 + " " + param3 + " " + param4;
     }
 }
--- java-reflection-method/src/main/java/eas/com/SomeInterface.java
+++ java-reflection-method/src/main/java/eas/com/SomeInterface.java
@@ -2,5 +2,5 @@ package eas.com;
 
 public interface SomeInterface {
 
-    String someMethod(String param1, String param2, String param3);
+    String someMethod(String param1, String param2, String param3, String param4);
 }
